fdb30b5f874b42d4ad97c8008e892616308e0fe0
baontq23 <baooshacker@gmail.com>
2023-04-07 03:47:50 +0700

    lab7

---
+++ lab7/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: "nodeauthsecret",
+  database:
+    "mongodb+srv://baontqph23876:DSOsu52y4iCBtKxR@cluster0.eemwnqy.mongodb.net/CP17305?retryWrites=true&w=majority",
+};
